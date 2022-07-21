d2127356f3bf20d0ebb61e220a408bd6e018f910
21016412 <21016412@myrp.edu.sg>
2022-07-21 12:43:46 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21016412/demotodolist/CustomAdapter.java
@@ -0,0 +1,50 @@
+package sg.edu.rp.c346.id21016412.demotodolist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+import sg.edu.rp.c346.id21016412.demotodolist.ToDoItem;
+
+public class CustomAdapter extends ArrayAdapter<ToDoItem> {
+
+    Context parent_context;
+    int layout_id;
+    ArrayList<ToDoItem> toDoList;
+
+    public CustomAdapter(Context context, int resource,  ArrayList<ToDoItem> objects) {
+        super(context, resource, objects);
+
+        parent_context = context;
+        layout_id = resource;
+        toDoList = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)
+                parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(layout_id, parent, false);
+
+        TextView tvName = rowView.findViewById(R.id.tvName);
+        TextView tvDate = rowView.findViewById(R.id.tvDate);
+
+        ToDoItem currentItem = toDoList.get(position);
+
+        tvName.setText(currentItem.getTitle());
+        tvDate.setText(currentItem.getDate());
+
+
+        return rowView;
+    }
+
+
+}
